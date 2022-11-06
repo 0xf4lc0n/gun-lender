c69d780ed8dedfcc058fc08608eb0477c080c389
0xf4lc0n <pmakowiecki0@gmail.com>
2022-11-06 14:13:36 +0100

    feat: add functionality requested in #3

--- src/main/java/gunlender/Main.java
+++ src/main/java/gunlender/Main.java
@@ -1,8 +1,10 @@
 package gunlender;
 
+import gunlender.domain.exceptions.RepositoryException;
 import gunlender.infrastructure.database.AmmoRepository;
 import gunlender.infrastructure.database.GunRepository;
 import gunlender.infrastructure.database.UserRepository;
+import gunlender.server.routes.HealthCheckHandler;
 import io.javalin.Javalin;
 import org.slf4j.LoggerFactory;
 
@@ -15,10 +17,18 @@ public class Main {
         var ammoRepo = new AmmoRepository(connectionStr);
         var gunRepo = new GunRepository(connectionStr);
 
-        userRepo.migrate();
-        ammoRepo.migrate();
-        gunRepo.migrate();
+        try {
+            userRepo.migrate();
+            ammoRepo.migrate();
+            gunRepo.migrate();
+        } catch (RepositoryException e) {
+            logger.error("Cannot migrate repository", e);
+            System.exit(1);
+        }
 
-        var app = Javalin.create().get("/", ctx -> ctx.result("Hello world")).start(8080);
+
+        var app = Javalin.create();
+        app.get("health_check", new HealthCheckHandler());
+        app.start(8080);
     }
 }

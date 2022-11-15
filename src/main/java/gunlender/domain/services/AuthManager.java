514f756f3eaf123e8dfffb6f2472679f96acc4db
0xf4lc0n <pmakowiecki0@gmail.com>
2022-11-15 23:47:08 +0100

    chore: add missing AuthManager for #5 and #4

---
+++ src/main/java/gunlender/domain/services/AuthManager.java
@@ -0,0 +1,53 @@
+package gunlender.domain.services;
+
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import io.javalin.security.AccessManager;
+import io.javalin.security.RouteRole;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Locale;
+import java.util.Set;
+
+public class AuthManager implements AccessManager {
+
+    private final JwtService jwtService;
+
+    public AuthManager(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+
+    @Override
+    public void manage(@NotNull Handler handler, @NotNull Context ctx, @NotNull Set<? extends RouteRole> set) throws Exception {
+        var userRole = getRole(ctx);
+
+        if (set.contains(userRole)) {
+            handler.handle(ctx);
+        } else {
+            ctx.status(401).result("Unauthorized");
+        }
+    }
+
+    private Role getRole(Context ctx) {
+        var jwt = ctx.header("Authorization");
+
+        if (jwt != null) {
+            return roleFromString(jwtService.getRole(jwt).orElse("anyone"));
+        }
+
+        return Role.ANYONE;
+    }
+
+    public enum Role implements RouteRole {
+        ANYONE, STANDARD_USER, ADMINISTRATOR
+    }
+
+    private Role roleFromString(String role) {
+        return switch (role.toLowerCase(Locale.ROOT)) {
+            case "standard" -> Role.STANDARD_USER;
+            case "administrator" -> Role.ADMINISTRATOR;
+            default -> Role.ANYONE;
+        };
+    }
+}
